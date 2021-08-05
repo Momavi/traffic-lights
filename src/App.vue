eb1a09910f8fe1a39465a85f48884710c8d26fc2
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-05 13:02:17 +0300

    clear project
    Remove all unnecessary components from the project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TraficLights/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TraficLights from './components/TraficLights.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TraficLights
   }
 }
 </script>
