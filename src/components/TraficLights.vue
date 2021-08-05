ffe2bd33af4b7dcf0373f4e16b30bdc5f1b7c173
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-05 16:18:35 +0300

    Add traffic light
    Add traffic light and SCSS styles

--- src/components/TraficLights.vue
+++ src/components/TraficLights.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="lights">
-
+    <div class="lights__wrapper">
+      <div class="lights__green circle">
+        <img class="lights__green-img" :src="lightWalk" alt="walking" />
+      </div>
+      <div class="lights__yellow circle">
+        <img class="lights__yellow-img" :src="lightStanding" alt="standing" />
+      </div>
+      <div class="lights__red circle">
+        <img class="lights__red-img" :src="lightStop" alt="stop" />
+      </div>
+    </div>
+    <div class="lights__stick"></div>
   </div>
 </template>
 
 <script>
+import lightWalk from "@/assets/lights-walk.svg";
+import lightStanding from "@/assets/lights-standing.svg";
+import lightStop from "@/assets/lights-stop.svg";
+
 export default {
-  name: 'TraficLights',
-  props: {
-  }
-}
+  name: "TraficLights",
+  props: {},
+  setup() {
+    return {
+      lightWalk,
+      lightStanding,
+      lightStop,
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "./TraficLights.module.scss";
 </style>
